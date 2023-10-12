56a62990cc807e94bcb7cc7b807170557582511a
Syed Ahamed <SYEDAHAMED29>
2023-10-12 23:02:26 +0530

    Fixed an error

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,9 @@ function Hero() {
    return (
       <div className="xl:w-[1200px] lg:w-[1000px] md:w-[768px] sm:w-[600px] w-[320px] m-auto">
          <section className="flex  flex-col  xl:items-center lg:items-center md:items-center sm:items-center my-7 ">
-            <HeroHeading />
+            <h1 className="lg:text-5xl xl:text-5xl md:text-4xl sm:text-3xl text-lg font-bold xl:mt-16 lg:mt-14 md:mt-7 ">
+               Greetings, I'm <span className="text-custom-blue">SYED AASHIQ AHAMED</span>
+            </h1>
             <p className="text-sm my-5 xl:my-10 lg:my-9 md:my-6 sm:my-5 w-[250px] xl:w-[630px] lg:w-[600px] md:w-[500px] sm:w-[400px] xl:text-center lg:text-center md:text-center sm:text-center text-left xl:text-2xl lg:text-2xl md:text-xl sm:text-lg  text-custom-grey">
                A Web Developer at heart, with a knack for creating elegant Web Applications that
                cater to unique requirements.
